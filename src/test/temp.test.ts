6c87f506d7d5e1ea8967366dca0b6ea5183e272e
YejinHwang-D <hyj3463@naver.com>
2023-05-17 03:14:01 +0900

    feat: renderHook 사용 hooks return값 불러오기기 * @testing-library/react-hooks의 버전을 낮춰야 버그가 해결됨 * useScroll의 useRef 타입 인자는 어떻게 넘겨야 하는지 해결하지 못함

--- src/test/temp.test.ts
+++ src/test/temp.test.ts
@@ -1,5 +1,59 @@
-it("Suggestion API correctly", async () => {
-  expect(5).toBe(5);
+/**
+ * @jest-environment jsdom
+ */
+
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+
+import { renderHook } from '@testing-library/react-hooks';
+import useFocus from "../hooks/useFocus";
+import usePost from "../hooks/usePost";
+import useScroll from "../hooks/useScroll";
+import { useState } from "react";
+
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}))
+
+const hooksMock = {
+  postArgs: {
+    setIsLoading: useState,
+    setTodos: useState,
+    setInputText: useState,
+    setIsTyping: useState,
+    inputText: "te",
+    ref: document.querySelector(".input-text"),
+    formRef: document.querySelector(".form-container"),
+  }
+}
+
+describe("hooks testing", () => {
+  test('useFocus', () => {
+    const { result } = renderHook(() => useFocus());
+    console.log(result.current);
+  });
+
+  test('usePost', () => {
+    const { result } = renderHook(
+      (initialProps: any) => usePost(initialProps),
+      { initialProps: hooksMock.postArgs }
+    );
+    console.log(result.current);
+  });
+
+  test('useScroll', () => {
+
+  });
+
+  test("useSearch", () => {
+
+  });
+
+  test("useThrottle", () => {
+
+  });
 })
 
-export { }
+
