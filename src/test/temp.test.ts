19437f7751d12fbd1fe82d6e31235fc6c751c4ba
YejinHwang-D <hyj3463@naver.com>
2023-05-17 12:45:11 +0900

    test: hooks의 결과값이 제대로 반환되는지 확인인

--- src/test/temp.test.ts
+++ src/test/temp.test.ts
@@ -8,14 +8,20 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useFocus from "../hooks/useFocus";
 import usePost from "../hooks/usePost";
-import useScroll from "../hooks/useScroll";
+// import useScroll from "../hooks/useScroll";
 import { useState } from "react";
-
+// import useSearch from '../hooks/useSearch';
+import { useDebounce } from "../hooks/useDebounce";
+import { INPUT_DELAY_TIME } from '../constants';
 
 jest.mock('react', () => ({
   ...jest.requireActual('react'),
   useState: jest.fn(),
-}))
+}));
+
+jest.mock("../hooks/useDebounce", () => {
+  debounce: jest.fn(() => Promise.resolve());
+})
 
 const hooksMock = {
   postArgs: {
@@ -26,10 +32,34 @@ const hooksMock = {
     inputText: "te",
     ref: document.querySelector(".input-text"),
     formRef: document.querySelector(".form-container"),
+  },
+  searchArgs: {
+    setInputText: useState,
+    setIsLoading: useState,
+    setIsTyping: useState,
+    setSuggestList: useState,
+    params: {
+      current: {
+        q: "te",
+        page: 1,
+        limit: 10
+      }
+    },
+    isMore: {
+      current: false
+    },
   }
 }
 
 describe("hooks testing", () => {
+  test('useDebounce', () => {
+    const { result } = renderHook(() => {
+      const debounce = useDebounce(INPUT_DELAY_TIME);
+      return debounce(debounce)
+    });
+    console.log(result)
+  });
+
   test('useFocus', () => {
     const { result } = renderHook(() => useFocus());
     console.log(result.current);
